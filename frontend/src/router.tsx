4bb6a73e0c776889b2e982fe100ae0e91d663693
franco-orozco <franco.nahuel.orozco@gmail.com>
2026-06-17 15:47:02 -0300

    se borro algunas carpetas, y se creo el archivo router.tsx paracontrolar los componente

---
+++ frontend/src/router.tsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+
+
+export default function Router(){
+
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route>
+                    
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+
+
+
+
